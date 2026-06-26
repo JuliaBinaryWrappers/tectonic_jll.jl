48fcc756249e893bcf1007b380ce54065a22b799
jlbuild <juliabuildbot@gmail.com>
2026-06-26 17:49:41 +0000

    tectonic_jll build 0.16.9+0

--- src/tectonic_jll.jl
+++ src/tectonic_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("tectonic")
-JLLWrappers.@generate_main_file("tectonic", UUID("d7dd28d6-a5e6-559c-9131-7eb760cdacc5"))
+JLLWrappers.@generate_main_file("tectonic", Base.UUID("d7dd28d6-a5e6-559c-9131-7eb760cdacc5"))
 end  # module tectonic_jll
